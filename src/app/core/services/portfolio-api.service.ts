c1ed7758427f80d5b5310f80d73a2a77ecbb9f4f
yogitak <yogitakatte25@gmail.com>
2025-09-01 19:09:32 +0530

    updates email

--- src/app/core/services/portfolio-api.service.ts
+++ src/app/core/services/portfolio-api.service.ts
@@ -79,6 +79,7 @@ export interface PortfolioCompanyResponse {
   acquisitionTargets: AcquisitionTarget[];
 }
 
+
 export interface TargetCompanyResponse {
   targetCompany: TargetCompanySynergy;
   buyerCompanyId: string;
